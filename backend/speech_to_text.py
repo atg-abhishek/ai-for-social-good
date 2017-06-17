6be3b8c56d9059642d5a4737364537936003d0b8
Andi Rayhan Chibrandy <andi.chibrandy@mail.mcgill.ca>
2017-06-17 17:05:32 -0400

    stt module

---
+++ backend/speech_to_text.py
@@ -0,0 +1,18 @@
+
+import json
+from os.path import join, dirname
+from watson_developer_cloud import SpeechToTextV1
+
+speech_to_text = SpeechToTextV1(
+    username='23d436aa-5ad1-4f18-b143-35298ff1836f',
+    password='8h2CmnJumDb5',
+    x_watson_learning_opt_out=False
+)
+
+filename = join(dirname(__file__), './Untitled.wav')
+
+with open(filename, 'rb') as audio_file:
+    print(json.dumps(speech_to_text.recognize(
+        audio_file, content_type='audio/wav', timestamps=True,
+        word_confidence=True),
+        indent=2))
